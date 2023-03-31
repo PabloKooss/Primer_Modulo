697fbffd27fafa018f696e6325b77a0b1d9a9ad9
Pablo-Lap <ximexkp@gmail.com>
2023-03-30 19:21:10 -0600

    Create primo.py

---
+++ primo.py
@@ -0,0 +1,11 @@
+print("Escribe un numero igual o menor que cien")
+num = int(input())
+def es_primo(num, n=2):
+    if n >= num:
+        print("Es primo")
+        return True
+    elif num % n != 0:
+        return es_primo(num, n + 1)
+    else:
+        print("No es primo", n, "es divisor")
+        return False
